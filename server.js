26e83b88899f18a64eb6b8d033ec99082a3099bb
Phil Johnston <johnstpd@gmail.com>
2026-02-13 14:05:36 -0800

    adding click stuff

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const os = require('os');
 
 const express = require('express');
 const figlet = require('figlet');
@@ -8,6 +9,22 @@ const app = express();
 const PORT = 3000;
 
 const displayText = process.env.DISPLAY_TEXT || 'No .ENV Present!';
+
+function getLocalIpAddress() {
+  const interfaces = os.networkInterfaces();
+  for (const name of Object.keys(interfaces)) {
+    for (const iface of interfaces[name]) {
+      if (iface.family === 'IPv4' && !iface.internal) {
+        return iface.address;
+      }
+    }
+  }
+  return null;
+}
+
+function clickableUrl(url) {
+  return `\x1b]8;;${url}\x07${url}\x1b]8;;\x07`;
+}
 
 function escapeHtml(text) {
   return text
@@ -28,7 +45,14 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`ASCII Art 3D Demo running at http://localhost:${PORT}`);
+  const localUrl = `http://localhost:${PORT}`;
+  const ip = getLocalIpAddress();
+  const networkUrl = ip ? `http://${ip}:${PORT}` : null;
+
+  console.log(`ASCII Art 3D Demo running at ${clickableUrl(localUrl)}`);
+  if (networkUrl) {
+    console.log(`  Network: ${clickableUrl(networkUrl)} (${ip})`);
+  }
   console.log(`Displaying: "${displayText}"`);
   console.log('Press Ctrl+C to stop');
 });
